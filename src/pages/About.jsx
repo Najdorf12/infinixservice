2c355672a54bfa452c1b0914bf6a2a32c954081d
Najdorf12 <agustin.morro@gmail.com>
2024-10-22 19:43:18 -0300

    add styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,12 +9,12 @@ const About = () => {
         id="second_section"
         className="w-full h-auto font-title overflow-hidden relative flex flex-col justify-between pb-12 2xl:min-h-screen xl:pb-16 2xl:pb-24"
       >
-        <div className=" z-50 flex gap-2 flex-col justify-center items-center py-12 ">
+        <div className=" z-50 flex gap-2 flex-col justify-center items-center py-12 xl:gap-4 ">
           <div
             id="box-glass2"
-            className="border-[2px] border-pastel2 flex w-[91%] max-w-[700px] h-[180px] z-50 rounded-2xl md:h-[200px] xl:max-w-[900px] xl:h-[230px]  md:justify-between 2xl:max-w-[950px] "
+            className="border-[2px] border-stone-400 flex w-[91%] max-w-[700px] h-[180px] z-50 rounded-2xl md:h-[200px] xl:max-w-[900px] xl:h-[230px]  md:justify-between 2xl:max-w-[950px] shadow-lg shadow-zinc-600 bg-gradient-to-tl from-teal-800 via-zinc-800 to-zinc-800"
           >
-            <article className="font-text3 text-balance flex flex-col pl-3 pt-2 relative text-zinc-700 xl:pt-4 xl:pl-4">
+            <article className="font-text3 text-balance flex flex-col pl-3 pt-2 relative text-zinc-400 xl:pt-4 xl:pl-6">
               <h6 className="text-lg font-medium md:text-xl text-primary xl:text-2xl">
                 Soluciones rápidas y eficientes
               </h6>
@@ -22,9 +22,9 @@ const About = () => {
                 Tecnología de punta para identificar y resolver cualquier
                 problema
               </p>
-              <button className="mt-8 flex justify-start items-center gap-1 text-base text-zinc-600 xl:text-lg">
+              <button className="mt-8 flex justify-start items-center gap-1 text-base text-zinc-200 xl:text-xl ">
                 Ver más
-                <i className="bx bx-chevron-right text-2xl text-primary xl:text-3xl"></i>
+                <i className="bx bx-chevron-right text-2xl text-primary xl:text-4xl"></i>
               </button>
             </article>
             <figure className="absolute right-0 -bottom-2 z-50 w-[130px] md:w-[170px] md:relative   md:self-end md:mt-4 xl:w-[230px] xl:right-6 ">
@@ -32,16 +32,16 @@ const About = () => {
             </figure>
           </div>
 
-          <section className="flex w-full justify-center gap-2 z-50 ">
+          <section className="flex w-full justify-center gap-2 z-50 xl:gap-3">
             <div
               id="box-glass2"
-              className="border-[2px] border-pastel2 w-[45%] h-[200px] rounded-2xl max-w-[348px] xl:max-w-[450px] xl:h-[220px]"
+              className="border-[2px] border-stone-400 w-[45%] h-[200px] rounded-2xl max-w-[348px] xl:max-w-[450px] xl:h-[220px] 2xl:max-w-[470px] shadow-lg shadow-zinc-600 bg-gradient-to-tl from-teal-800 via-zinc-800 to-zinc-800"
             >
               <article className="font-text3 text-balance flex flex-col pl-3 pt-2 xl:pt-4 xl:pl-4">
                 <h6 className="text-lg font-medium md:text-xl text-primary  xl:text-2xl">
                   Soporte 24/7
                 </h6>
-                <p className="text-sm mt-1 text-zinc-700 md:text-base pr-3 xl:text-lg xl:max-w-[240px] xl:mt-3">
+                <p className="text-sm mt-1 text-zinc-400 md:text-base pr-3 xl:text-lg xl:max-w-[240px] xl:mt-3">
                   Equipo listo para asistirte cuando lo necesites.
                 </p>
               </article>
@@ -56,13 +56,13 @@ const About = () => {
 
             <div
               id="box-glass2"
-              className="border-[2px] border-pastel2 w-[45%] h-[200px]  rounded-2xl max-w-[348px] xl:max-w-[450px] xl:h-[220px] "
+              className="border-[2px] border-stone-400 w-[45%] h-[200px]  rounded-2xl max-w-[348px] xl:max-w-[450px] xl:h-[220px] 2xl:max-w-[470px] shadow-lg shadow-zinc-600 bg-gradient-to-tl from-teal-800 via-zinc-800 to-zinc-800"
             >
               <article className="font-text3 text-balance flex flex-col pl-3 pt-2 xl:pt-4 xl:pl-4">
                 <h6 className="text-lg font-medium md:text-xl text-primary xl:text-2xl">
                   Garantía
                 </h6>
-                <p className="text-sm mt-1 text-zinc-700  md:text-base xl:mt-3 xl:text-lg">
+                <p className="text-sm mt-1 text-zinc-400  md:text-base xl:mt-3 xl:text-lg">
                   Nos especializamos en una amplia gama de reparaciones
                 </p>
               </article>
